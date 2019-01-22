3f15d50aec121538fce810085a5ce65f05db32cd
Luiz Soares <luizsoares@Mac-mini-de-Luiz.local>
2019-01-22 12:54:17 -0200

    devportfolio

--- js/user.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    $(".teste").click(function () {
-        var conteudo = $(this).html();
-        conteudo += "teste";
-        conteudo.replace("")
-
-    });
-});
